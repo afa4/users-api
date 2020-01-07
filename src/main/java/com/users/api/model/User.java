f729dd7523bc18fdf21f91264e660a5d9164459b
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-06 23:25:37 -0300

    Fixing constructor problems

--- src/main/java/com/users/api/model/User.java
+++ src/main/java/com/users/api/model/User.java
@@ -1,5 +1,6 @@
 package com.users.api.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Getter
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class User {

344fb52570b00981072c0b5cc889a1d8210603ba
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-06 23:07:14 -0300

    Including builder annotation

--- src/main/java/com/users/api/model/User.java
+++ src/main/java/com/users/api/model/User.java
@@ -1,11 +1,15 @@
 package com.users.api.model;
 
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
-@Data
+@Getter
+@Builder
+@NoArgsConstructor
 @Entity
 public class User {
     @Id
@@ -14,6 +18,11 @@ public class User {
     private String name;
     private String email;
     private String password;
-    @OneToMany
+    @OneToMany(cascade = CascadeType.PERSIST)
     private List<Phone> phones;
+
+    public void setPhones(List<Phone> phones) {
+        phones.forEach(phone -> phone.setUser(this));
+        this.phones = phones;
+    }
 }

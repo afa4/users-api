62863e46ef17d8161e6d7ea8bb7d2fc898a06191
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-06 23:10:46 -0300

    Rest controller for registering new users

---
+++ src/main/java/com/users/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.users.api.controller;
+
+import com.users.api.exception.UserExistsException;
+import com.users.api.model.dto.UserDTO;
+import com.users.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody UserDTO userDTO) {
+        try {
+            userService.create(userDTO);
+                return new ResponseEntity(HttpStatus.CREATED);
+        } catch (UserExistsException e) {
+            return new ResponseEntity(HttpStatus.CREATED);
+        }
+
+    }
+}

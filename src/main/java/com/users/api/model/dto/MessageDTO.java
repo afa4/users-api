673ea3ae6a072d149dbc77dda1310334f3ee62d7
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-07 00:27:32 -0300

    Getters and setters for json serialization

--- src/main/java/com/users/api/model/dto/MessageDTO.java
+++ src/main/java/com/users/api/model/dto/MessageDTO.java
@@ -1,7 +1,11 @@
 package com.users.api.model.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class MessageDTO {
     String message;

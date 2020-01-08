996ca01e4f58e6d2a446d95da81ca8f0d78610bf
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-08 00:28:45 -0300

    Updating tables field types

--- tables.sql
+++ tables.sql
@@ -3,10 +3,13 @@ create table USER
     ID BIGINT identity
         constraint USER_PK
             primary key,
-    NAME VARCHAR(100),
-    EMAIL VARCHAR(100)
-        unique,
-    PASSWORD VARCHAR(100)
+    CREATED    TIMESTAMP NOT NULL,
+    MODIFIED   TIMESTAMP,
+    LAST_LOGIN TIMESTAMP,
+    NAME       VARCHAR(255) NOT NULL,
+    EMAIL      VARCHAR(255) UNIQUE NOT NULL,
+    PASSWORD   VARCHAR(255) NOT NULL,
+    TOKEN      VARCHAR(255) UNIQUE NOT NULL
 );
 
 create table PHONE
@@ -14,10 +17,9 @@ create table PHONE
     ID BIGINT identity
         constraint PHONE_PK
             primary key,
-    DDD VARCHAR(3),
-    NUMBER VARCHAR(10),
+    DDD VARCHAR(3) NOT NULL,
+    NUMBER VARCHAR(10) NOT NULL,
     USER_ID BIGINT
         constraint PHONE_USER_ID_FK
             references USER
 );
-

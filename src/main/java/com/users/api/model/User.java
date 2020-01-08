57cada1cfe0ba75e62edc0759121505f07a47cb9
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-08 00:30:35 -0300

    User new attributes

--- src/main/java/com/users/api/model/User.java
+++ src/main/java/com/users/api/model/User.java
@@ -1,14 +1,15 @@
 package com.users.api.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,10 +17,15 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue
-    private Long Id;
+    private Long id;
+    private LocalDateTime created;
+    private LocalDateTime modified;
+    private LocalDateTime lastLogin;
     private String name;
     private String email;
     private String password;
+    private String token;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
     private List<Phone> phones;
 

69a9a9e4b90a3c91f1e056d7c99ef475111ee7ad
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-09 01:41:46 -0300

    Changing exception name

--- src/main/java/com/users/api/controller/LoginController.java
+++ src/main/java/com/users/api/controller/LoginController.java
@@ -1,10 +1,11 @@
 package com.users.api.controller;
 
-import com.users.api.exception.EmailOrPasswordIncorrectException;
+import com.users.api.exception.UnauthorizedException;
 import com.users.api.model.dto.LoginDTO;
 import com.users.api.model.dto.MessageDTO;
 import com.users.api.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +22,8 @@ public class LoginController {
     public ResponseEntity login(LoginDTO loginDTO) {
         try {
             return ResponseEntity.ok(loginService.login(loginDTO.getEmail(), loginDTO.getPassword()));
-        } catch (EmailOrPasswordIncorrectException e) {
-            return ResponseEntity.badRequest().body(new MessageDTO(e.getMessage()));
+        } catch (UnauthorizedException e) {
+            return new ResponseEntity(new MessageDTO(e.getMessage()), HttpStatus.UNAUTHORIZED);
         }
     }
 }

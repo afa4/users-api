bf8d943a2e3864c1ca16c20fa135ef5ebbd085ea
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-09 01:43:03 -0300

    Including Authorization validation in create user endpoint

--- src/main/java/com/users/api/controller/UserController.java
+++ src/main/java/com/users/api/controller/UserController.java
@@ -1,17 +1,17 @@
 package com.users.api.controller;
 
 import com.users.api.exception.EmailAlreadyExistsException;
+import com.users.api.exception.ExpiredTokenException;
+import com.users.api.exception.UnauthorizedException;
 import com.users.api.model.dto.MessageDTO;
 import com.users.api.model.dto.UserCreatedDTO;
 import com.users.api.model.dto.UserDTO;
+import com.users.api.service.AuthenticationService;
 import com.users.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -19,10 +19,13 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticationService authenticationService;
 
     @PostMapping
-    public ResponseEntity create(@RequestBody UserDTO userDTO) {
+    public ResponseEntity create(@RequestBody UserDTO userDTO, @RequestHeader(value = "Authorization") String authorization) {
         try {
+            authenticationService.validateAuthorization(authorization);
             var user = userService.create(userDTO);
             var response = new UserCreatedDTO(user.getUuid(),
                     user.getCreated(),
@@ -30,6 +33,8 @@ public class UserController {
                     user.getLastLogin(),
                     user.getToken());
             return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (UnauthorizedException | ExpiredTokenException e) {
+            return new ResponseEntity(new MessageDTO(e.getMessage()), HttpStatus.UNAUTHORIZED);
         } catch (EmailAlreadyExistsException e) {
             return new ResponseEntity<>(new MessageDTO(e.getMessage()), HttpStatus.CONFLICT);
         } catch(Exception e) {

5704d9fe06f49b38ff7842a7b0dd4ac0bcf1d227
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-09 01:41:15 -0300

    Find by token method for Users

--- src/main/java/com/users/api/repository/UserRepository.java
+++ src/main/java/com/users/api/repository/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByEmailAndPassword(String email, String password);
+    Optional<User> findByToken(String token);
 }

6e902186948c6f0258c293b68eca11d0c879ea47
arthurfaraujo <afa4@cin.ufpe.br>
2020-01-10 00:15:55 -0300

    Root user sql insert

---
+++ root.sql
@@ -0,0 +1,9 @@
+INSERT INTO "user"(id, created, name, password, token, uuid)
+VALUES(
+0,
+'root@root',
+'root',
+'4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2',
+'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb290QHJvb3QiLCJuYW1lIjoicm9vdCIsImlhdCI6MTU3ODYxMjk4OX0.K6Ob9dK00u2xN6r1sYh7K82hhe4w0F_WT6GvG3AqQ0r1tV1Yklc7fJD3pdWWviClISrgnwFkNO230ceMEYldkQ',
+'e14c0dc3-a395-478d-a769-729e8c114a6b'
+);
